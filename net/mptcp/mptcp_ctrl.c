acb6b0b97d943b098aa8b0bd6201e2cacce2404d
Christoph Paasch <christoph.paasch@uclouvain.be>
2013-10-15 08:56:45 +0200

    New MPTCP maintenance release
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- net/mptcp/mptcp_ctrl.c
+++ net/mptcp/mptcp_ctrl.c
@@ -1873,7 +1873,7 @@ void __init mptcp_init(void)
 		goto register_sysctl_failed;
 #endif
 
-	pr_info("MPTCP: Stable release v0.87.2");
+	pr_info("MPTCP: Stable release v0.87.3");
 
 	mptcp_init_failed = false;
 
